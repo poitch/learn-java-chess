9d4828f28bd06c9acdd9be1bf7a95be68cdcaea2
Jêrôme Poichet <jerome@kingofsofa.org>
2021-01-29 11:41:10 -0800

    Renaming panel to grid so it's easier to read the code.

--- src/ch/poit/chess/Chess.java
+++ src/ch/poit/chess/Chess.java
@@ -9,9 +9,9 @@ public class Chess {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Dimension dimension = new Dimension(800, 800);
-        JPanel panel = new JPanel(new GridLayout(8, 8));
-        panel.setPreferredSize(dimension);
-        panel.setBounds(0, 0, dimension.width, dimension.height);
+        JPanel grid = new JPanel(new GridLayout(8, 8));
+        grid.setPreferredSize(dimension);
+        grid.setBounds(0, 0, dimension.width, dimension.height);
         for(int i = 0; i < 64; i++)  {
             JPanel square = new JPanel();
             int row = i / 8;
@@ -20,10 +20,10 @@ public class Chess {
             } else {
                 square.setBackground(i % 2 == 0 ? Color.white : Color.blue );
             }
-            panel.add(square);
+            grid.add(square);
         }
 
-        mainFrame.setContentPane(panel);
+        mainFrame.setContentPane(grid);
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
