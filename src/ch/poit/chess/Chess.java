09e72c94c33607e3322e709bc6bbde67aefd723d
Jêrôme Poichet <jerome@kingofsofa.org>
2021-01-29 11:39:35 -0800

    003 Drawing Chess Board
    Replace the BorderLayout by a GridLayout of 8 columns and 8 rows.
    Use a for loop to add JPanel to that GridLayout
    Use the index of the loop to alternate the background color of those new panels.

--- src/ch/poit/chess/Chess.java
+++ src/ch/poit/chess/Chess.java
@@ -8,14 +8,23 @@ public class Chess {
         JFrame mainFrame = new JFrame("Chess");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel(new BorderLayout());
-        JButton button = new JButton();
-        button.setText("Click Me");
-        panel.add(button);
+        Dimension dimension = new Dimension(800, 800);
+        JPanel panel = new JPanel(new GridLayout(8, 8));
+        panel.setPreferredSize(dimension);
+        panel.setBounds(0, 0, dimension.width, dimension.height);
+        for(int i = 0; i < 64; i++)  {
+            JPanel square = new JPanel();
+            int row = i / 8;
+            if (row % 2 == 0) {
+                square.setBackground(i % 2 == 0 ? Color.blue : Color.white );
+            } else {
+                square.setBackground(i % 2 == 0 ? Color.white : Color.blue );
+            }
+            panel.add(square);
+        }
+
         mainFrame.setContentPane(panel);
         mainFrame.pack();
         mainFrame.setVisible(true);
-
-
     }
 }
