f17ac88466f73cff1d71235e99620f7f45187cc5
Jêrôme Poichet <jerome@kingofsofa.org>
2021-01-29 10:52:23 -0800

    002 Show a simple window
    Replace the hello world with the creation of a JFrame containing a JPanel containing a JButton that does nothing :laughing:

--- src/ch/poit/chess/Chess.java
+++ src/ch/poit/chess/Chess.java
@@ -1,7 +1,21 @@
 package ch.poit.chess;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Chess {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        JFrame mainFrame = new JFrame("Chess");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        JButton button = new JButton();
+        button.setText("Click Me");
+        panel.add(button);
+        mainFrame.setContentPane(panel);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
+
     }
 }
